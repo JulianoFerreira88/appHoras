537cf1add12491a087a7c2a4307ac16aeec824af
Juliano <julianoferreira336@gmail.com>
2020-07-13 22:30:52 -0300

    Criacao estrutura mvc com algumas classes

--- src/model/entities/Registro.java
+++ src/model/entities/Registro.java
@@ -6,29 +6,74 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class Registro {
-    private long id;
-    private Date date;
+    private int id;
+    private String hora;
+    private String data;
 
     public Registro() {
     }
 
-    public long getId() {
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Registro(int id, String time) {
+        this.id = id;
+        this.hora = time;
+    }
+
+    public Registro(String hora) {
+        this.hora = hora;
+    }
+
+    public int getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(int id) {
         this.id = id;
     }
 
-    public Date getDate() {
-        return date;
+    public String getHora() {
+        return hora;
     }
 
-    public void setDate(Date date) {
-        this.date = date;
+    public void setHora(String hora) {
+        this.hora = hora;
     }
 
-    public long getLong() {
-        return this.date.getTime();
+    public void setTime(String time) {
+        this.hora = time;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 31 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Registro other = (Registro) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return hora;
     }
 }
