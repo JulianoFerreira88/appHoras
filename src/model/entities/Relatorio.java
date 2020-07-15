1e8edd1b2f07405ae00d9452e227d60e29ac6e84
Juliano <julianoferreira336@gmail.com>
2020-07-14 21:04:54 -0300

    Inicio da Classe Relatorio

---
+++ src/model/entities/Relatorio.java
@@ -0,0 +1,11 @@
+package model.entities;
+
+import java.util.List;
+
+public class Relatorio {
+    public static String horasTrabalhadas(List<Registro> registros) {
+        StringBuilder res = new StringBuilder();
+
+        return res.toString();
+    }
+}
